ff26c8d28f872099766d9a84baa41b8d126f7cac
Nima Namazi <nimanamazi1994@gmail.com>
2025-10-11 19:12:20 +0330

    fix: import issue

--- src/app/api/signout/route.ts
+++ src/app/api/signout/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import { createClient as createSupabaseServer } from '../../utils/supabase/server'
+import { createServer as createSupabaseServer } from '../../utils/supabase/server'
 
 export async function POST() {
   const supabase = await createSupabaseServer()
